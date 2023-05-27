fbabe203b426fd23484b76b83eefde2f63800216
tskinner429 <tskinner429@gmail.com>
2023-05-27 06:42:45 -0400

    product

--- models/index.js
+++ models/index.js
@@ -18,6 +18,11 @@ Category.hasMany(Product, {
 
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+
 
 // Tags belongToMany Products (through ProductTag)
 
