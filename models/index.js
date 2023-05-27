cc3f43356ea31b934de45bed5a5a40dddb7eaeb4
tskinner429 <tskinner429@gmail.com>
2023-05-27 06:41:07 -0400

    index

--- models/index.js
+++ models/index.js
@@ -5,6 +5,11 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
 
 // Categories have many Products
 
